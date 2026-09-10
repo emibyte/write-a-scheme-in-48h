e70a0bb03bc8fc25f86bfc329817c8d6350fa40a
Emily <fevymarine@gmail.com>
2026-09-10 17:57:44 +0200

    repo init and dev shell

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "dev env for 'Write yourself a scheme in 48 hours'";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+      ghc = pkgs.haskellPackages.ghcWithPackages (hp: [
+        hp.parsec
+      ]);
+    in {
+      formatter = pkgs.alejandra;
+      devShells.default = pkgs.mkShell {
+        packages = [
+          ghc
+          pkgs.haskellPackages.cabal-install
+          pkgs.haskell-language-server
+        ];
+      };
+    });
+}
