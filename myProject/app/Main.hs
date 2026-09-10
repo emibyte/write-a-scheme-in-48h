7d9caaf57f05fe26fb4c6ce244947f52c161701a
Emily <fevymarine@gmail.com>
2026-09-10 22:55:34 +0200

    some parse functions

--- myProject/app/Main.hs
+++ myProject/app/Main.hs
@@ -2,6 +2,7 @@ module Main (main) where
 
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
+import Control.Monad (liftM)
 
 data LispVal
   = Atom String
@@ -26,3 +27,23 @@ readExpr :: String -> String
 readExpr input = case parse (spaces >> symbol) "lisp" input of
   Left err -> "No match: " ++ show err
   Right val -> "Found value"
+
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = first : rest
+  return $ case atom of
+    "#t" -> Bool True
+    "#f" -> Bool False
+    _ -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
