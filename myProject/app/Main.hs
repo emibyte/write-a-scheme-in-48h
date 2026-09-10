3b05407bf60383ebdba7927fdde723ff4ab2b444
Emily <fevymarine@gmail.com>
2026-09-10 22:33:34 +0200

    until return values sub chapter

--- myProject/app/Main.hs
+++ myProject/app/Main.hs
@@ -1,4 +1,28 @@
 module Main (main) where
 
+import System.Environment
+import Text.ParserCombinators.Parsec hiding (spaces)
+
+data LispVal
+  = Atom String
+  | List [LispVal]
+  | DottedList [LispVal] LispVal
+  | Number Integer
+  | String String
+  | Bool Bool
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  (expr : _) <- getArgs
+  putStrLn (readExpr expr)
+
+symbol :: Parser Char
+symbol = oneOf "!#$%%|*+-/:<=>?@^_~"
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+readExpr :: String -> String
+readExpr input = case parse (spaces >> symbol) "lisp" input of
+  Left err -> "No match: " ++ show err
+  Right val -> "Found value"
